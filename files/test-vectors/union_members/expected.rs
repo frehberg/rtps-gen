5be055b442fe8f0e45772078e4902b91f756d675
Frank Rehberger <frehberg@gmail.com>
2019-01-13 18:18:43 +0100

    REFACTOR: rename test-vectors

--- files/test-vectors/union_members/expected.rs
+++ files/test-vectors/union_members/expected.rs
@@ -2,3 +2,19 @@
 use std::vec::Vec;
 #[allow(unused_imports)]
 use serde_derive::{Serialize, Deserialize};
+
+//
+//
+#[allow(dead_code)]
+#[allow(non_camel_case_types)]
+#[derive(Serialize, Deserialize)]
+#[derive(Clone, Debug)]
+pub enum Foo {
+    LABEL0(i32),
+    LABEL1(i16),
+    LABEL2(i16),
+    default(u8),
+}
+//
+// TODO custom de-/serializer
+//
