12c05cb13cae197fc4e234b0c2a1e477a4025aa1
Frank Rehberger <frehberg@gmail.com>
2019-01-14 01:06:17 +0100

    TEST: fix union_members

--- files/test-vectors/union_members/expected.rs
+++ files/test-vectors/union_members/expected.rs
@@ -10,10 +10,10 @@ use serde_derive::{Serialize, Deserialize};
 #[derive(Serialize, Deserialize)]
 #[derive(Clone, Debug)]
 pub enum Foo {
-    LABEL0(i32),
-    LABEL1(i16),
-    LABEL2(i16),
-    default(u8),
+    LABEL0{l: i32,},
+    LABEL1{s: i16,},
+    LABEL2{s: i16,},
+    default{o: u8,},
 }
 //
 // TODO custom de-/serializer
